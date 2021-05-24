ad4bb1eafed07a6fb03a4fe7de163f7e0ec8bcee
jhon-swai <jhonswai@gmail.com>
2021-05-24 09:51:19 +0300

    added flask cors package

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify
 
+from flask_cors import CORS
+
 # path to database 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,6 +19,7 @@ PASSWORD = 'default'
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
+CORS(app)
 
 
 
